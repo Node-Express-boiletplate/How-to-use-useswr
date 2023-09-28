09969a66156a3e948a433516f74d13415bd9e056
sagarstha254 <sagarstha254@gmail.com>
2023-09-28 17:27:13 +0545

    feat(UI):API created

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+const Home = () => {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch("http://localhost:3001");
+      const data = await response.json();
+      setUsers(data);
+    }
+    fetchUsers();
+  }, []);
+
+  if (!users) return <h2>Loading..</h2>;
+
+  return;
+  <>
+    <div>
+      {users.map((user, index) => {
+        return <h2 key={index}>{user.name}</h2>;
+      })}
+    </div>
+  </>;
+};
+
+export default Home;
